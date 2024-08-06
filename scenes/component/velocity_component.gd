0ad7c23100b3125b90a1e96f1f4fb5da899812fc
Sam Gray <samleegray@gmail.com>
2024-08-06 08:36:48 -0500

    Forced Errors and Warnings
    Now forcing errors and warnings for code, ensuring I static type everything.

--- scenes/component/velocity_component.gd
+++ scenes/component/velocity_component.gd
@@ -1,34 +1,35 @@
+class_name VelocityComponent
 extends Node
 
 @export var max_speed: int = 40
 @export var acceleration: float = 5
 
-var velocity = Vector2.ZERO
+var velocity: Vector2 = Vector2.ZERO
 
 
-func accelerate_to_player():
-	var owner_node2d = owner as Node2D
+func accelerate_to_player() -> void:
+	var owner_node2d: Node2D = owner
 	if owner_node2d == null:
 		return
 		
-	var player = get_tree().get_first_node_in_group("player") as Player
+	var player: Player = get_tree().get_first_node_in_group("player")
 	if player == null:
 		return
 		
-	var direction = (player.global_position - owner_node2d.global_position).normalized()
+	var direction: Vector2 = player.global_position.direction_to(owner_node2d.global_position) #(player.global_position - owner_node2d.global_position).normalized()
 	accelerate_in_direction(direction)
 
 
-func accelerate_in_direction(direction: Vector2):
-	var desired_velocity = direction * max_speed
+func accelerate_in_direction(direction: Vector2) -> void:
+	var desired_velocity: Vector2 = direction * max_speed
 	velocity = velocity.lerp(desired_velocity, (1 - exp(-acceleration * get_process_delta_time())))
 
 
-func decelerate():
+func decelerate() -> void:
 	accelerate_in_direction(Vector2.ZERO)
 
 
-func move(character_body: CharacterBody2D):
+func move(character_body: CharacterBody2D) -> void:
 	character_body.velocity = velocity
 	character_body.move_and_slide()
 	velocity = character_body.velocity
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,32 +1,25 @@
 extends CharacterBody2D
 class_name Player
 
-@onready var visuals = $Visuals
-@onready var velocity_component = $VelocityComponent
-
-var number_colliding_bodies = 0
-var base_speed = 0
-
-func _ready():
-	base_speed = velocity_component.max_speed
+@onready var visuals: Node2D = $Visuals
+@onready var velocity_component: VelocityComponent = $VelocityComponent
 
 
-func _process(delta):
-	var movement_vector = get_movement_vector()
-	var direction = movement_vector.normalized()
+func _process(delta: float) -> void:
+	var movement_vector: Vector2 = get_movement_vector()
+	var direction: Vector2 = movement_vector.normalized()
 	
 	velocity_component.accelerate_in_direction(direction)
 	
 	velocity_component.move(self)
 	
-	var move_sign = sign(movement_vector.x)
+	var move_sign: int = sign(movement_vector.x)
 	if move_sign != 0:
 		visuals.scale = Vector2(move_sign, 1)
 
 
-func get_movement_vector():
-	var x_movement = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	var y_movement = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+func get_movement_vector() -> Vector2:
+	var x_movement: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	var y_movement: float = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	return Vector2(x_movement, y_movement)
-
